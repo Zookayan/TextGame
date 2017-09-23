ff8f279d0a4f2847369198243dda7573be44bf03
Harris <32201697+harrissknox@users.noreply.github.com>
2017-09-23 18:07:44 -0500

    Still making changes today, but committing

--- Project3/Game.h
+++ Project3/Game.h
@@ -20,7 +20,18 @@ public:
 	void mainMenu();
 
 	//Accessors
-	inline bool getPlaying() const { return this->playing; }
+	inline const double& getX() const { return this->xPos; } 
+	inline const double& getY() const { return this->yPos; } 
+	inline const std::string& getName() const { return this->name; }
+	inline const int& getLevel() const { return this->level; } 
+	inline const int& getExp() const { return this->exp; }
+	inline const int& getExpNext() const { return this->expNext; }
+	inline const int& getHP() const { return this->hp; }
+	inline const int& getHPMax() const { return this->hpMax; }
+	inline const int& getStamina() const { return this->stamina; }
+	inline const int& getDamageMin() const { return this->damageMin; }
+	inline const int& getDamageMax() const { return this->damageMax; }
+	inline const int& getDefence() const { return this->20; }
 
 	//Modifiers
 
